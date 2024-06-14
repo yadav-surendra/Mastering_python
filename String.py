9dec815380f719b4c697ac0908f51d235b8533a6
yadav-surendra <138237820+yadav-surendra@users.noreply.github.com>
2024-06-14 23:43:18 +0530

    Update String.py

--- String.py
+++ String.py
@@ -7,3 +7,15 @@ print(s)
 print(m)
 a=s+m
 print(a)
+
+print(s)
+print(m)
+a=s+m
+print(a)
+
+
+a= int(input("enter the price"))
+s="the price of this book is "
+e= " Rs:"
+price = a+ 50
+print(s+e+str(price))
