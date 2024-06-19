07caeaa0018cf78c6350cf5d3648db38720f1f22
yadav-surendra <138237820+yadav-surendra@users.noreply.github.com>
2024-06-19 23:44:03 +0530

    Create loops-in-tuple.py

---
+++ loops-in-tuple.py
@@ -0,0 +1,15 @@
+a = ("apple","litchee","mango","papaya","grapes")
+
+for x in a:
+    print(x)
+    
+print("using for in range loop")   
+for i in range(len(a)):
+    print(a[i])
+    
+    
+print("using while loop")   
+i =0;
+while i< len(a):
+    print(a[i])
+    i+=1
