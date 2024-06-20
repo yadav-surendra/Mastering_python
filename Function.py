f84d635d5265a693308c3e0f6332ac6d150b0ebe
yadav-surendra <138237820+yadav-surendra@users.noreply.github.com>
2024-06-20 19:06:31 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,1 +1,24 @@
 
+# lets first start with the conventional Hello world function
+
+def myfun():
+    print("hello coders.")
+    print("whats upp..")
+    
+myfun()
+    
+def sons(*args):
+    print("my obedient son is:",args[2])
+    
+sons("ram","shyam","hari","vishnu")
+
+def key_func(first,second,third):
+    print("my first god name is:" + first)
+    
+key_func(first = "ram", second = "hari", third = "shyam")
+
+def key_wala(**args):
+    print("my fav god name is:" , args["second"])
+    
+key_wala(first = "ram", second = "hari", third = "shyam")
+
