8a0e36ed7f35b7f4a8b563242f41a071546f6d15
yadav-surendra <138237820+yadav-surendra@users.noreply.github.com>
2024-06-20 09:01:47 +0530

    Update Dictionary.py
    first commit

--- Dictionary.py
+++ Dictionary.py
@@ -1,1 +1,22 @@
+#dictionary in python
+""" stores data in key value format.
+
+"""
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+x=thisdict.get("model")
+print(x)
+print(thisdict["model"])
+
+a=thisdict.keys()
+b=thisdict.values()
+print(a,b)
+
+thisdict.update({"year": 2020})
+print(thisdict)
 
