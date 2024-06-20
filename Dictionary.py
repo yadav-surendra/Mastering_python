b00b07b0f2e16d4764d475eee95b6ca5e9340f9b
yadav-surendra <138237820+yadav-surendra@users.noreply.github.com>
2024-06-20 13:10:20 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -31,3 +31,22 @@ dict2 = mydict.copy()
 dict3 = dict(mydict)
 print(dict3)
 print(dict2)
+
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
